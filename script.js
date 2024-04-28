93d285d07add815128509cebfd9aaf1b9c2370d4
ABHIRAJ9064 <159516528+ABHIRAJ9064@users.noreply.github.com>
2024-04-28 13:33:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function firstWord(s) {
   // your code here
+	if(!s){
+		return '';
+	}
+	const initialWord = s.split(' ');
+	let firstWord = initialWord[0];
+	return firstWord;
 }
+
+
 
 // Do not change the code below
 
